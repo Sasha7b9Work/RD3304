9fbb36be8bdcf57dc631bc2ff6b837e33ba39c68
DESKTOP-UL3J1B2\Sasha <Sasha7b9@tut.by>
2024-06-27 08:55:15 +0300

    Возврат к состоянию до экспериментов с Mifare

--- sources/Milfare/src/Utils/String.cpp
+++
@@ -1,4 +0,0 @@
-// 2023/09/24 13:16:25 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
-#include "defines.h"
-#include "Utils/String.h"
-
--- sources/Milfare/src/defines.cpp
+++
@@ -1,3 +0,0 @@
-// 2022/7/13 9:46:48 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
-#include "defines.h"
-//#include <cstdio>
